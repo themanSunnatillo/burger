01e72edd62faea6c1a6789dffeb5be9896b3d2cc
themanSunnatillo <ytraderoearning@gmail.com>
2024-03-14 11:31:02 +0500

    done burger

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
 const burgerBtn = document.querySelector(".burger");
-
-burgerBtn.addEventListener("click", ()=>{
-    
-})
+const burgerBox = document.querySelector(".burger-box");
+const menuItems = document.querySelectorAll(".menu");
+burgerBtn.addEventListener("click", () => {
+  burgerBox.classList.toggle("active");
+  if (burgerBox.classList.contains("active")) {
+    menuItems.forEach((menuItem, index) => {
+      menuItem.classList.add("active" + (index + 1));
+    });
+  } else {
+    menuItems.forEach((menuItem, index) => {
+      menuItem.classList.remove("active" + (index + 1));
+    });
+  }
+});
